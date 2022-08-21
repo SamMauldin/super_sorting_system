df4626e463687eb8b63fbb241db87da1bf7b33d1
Sam Mauldin <sam@mauldin.me>
2022-08-21 16:35:34 +0000

    [gradient] Add display name to items

--- gradient/src/helpers/items.ts
+++ gradient/src/helpers/items.ts
@@ -4,74 +4,89 @@ import { McData } from "../common";
 
 export type ExtendedItem = Item & { prettyPrinted: string };
 
+const anvilName = (item: Item): string | null => {
+	const nbtDisplayNameJson = item.nbt?.value?.display?.value?.Name?.value;
+
+	try {
+		return JSON.parse(nbtDisplayNameJson).text;
+	} catch (_) {
+		return null;
+	}
+};
+
 const enchantmentData = (mcData: McData, item: Item): string | null => {
-  const nbtEnchantments =
-    item.nbt?.value?.Enchantments || item.nbt?.value?.StoredEnchantments;
-  if (!nbtEnchantments) return null;
+	const nbtEnchantments =
+		item.nbt?.value?.Enchantments || item.nbt?.value?.StoredEnchantments;
+	if (!nbtEnchantments) return null;
 
-  const enchantments = nbtEnchantments.value.value.map((enchant: any) => {
-    const name = enchant.id.value.split(":")[1];
-    const level = enchant.lvl.value;
+	const enchantments = nbtEnchantments.value.value.map((enchant: any) => {
+		const name = enchant.id.value.split(":")[1];
+		const level = enchant.lvl.value;
 
-    const enchantmentType = mcData.enchantments.get(name);
+		const enchantmentType = mcData.enchantments.get(name);
 
-    const displayName = enchantmentType?.displayName || name;
+		const displayName = enchantmentType?.displayName || name;
 
-    return `${displayName} ${level}`;
-  });
+		return `${displayName} ${level}`;
+	});
 
-  return enchantments.join(", ");
+	return enchantments.join(", ");
 };
 
 export const prettyPrint = (mcData: McData, item: Item): string => {
-  const itemType = mcData.items.get(item!.item_id)!;
+	const itemType = mcData.items.get(item!.item_id)!;
 
-  const enchants = enchantmentData(mcData, item);
+	const enchants = enchantmentData(mcData, item);
+	const anvilDisplayName = anvilName(item);
 
-  let text = itemType.displayName;
+	let text = itemType.displayName;
 
-  if (enchants) {
-    text += ` (${enchants})`;
-  }
+	if (anvilDisplayName) {
+		text += ` "${anvilDisplayName}"`;
+	}
 
-  return text;
+	if (enchants) {
+		text += ` (${enchants})`;
+	}
+
+	return text;
 };
 
 export const stackMatches = (
-  stackA: Item | null,
-  stackB: Item | null,
-  ignoreCount?: boolean
+	stackA: Item | null,
+	stackB: Item | null,
+	ignoreCount?: boolean
 ): boolean => {
-  if (stackA === null || stackB === null) return stackA === stackB;
+	if (stackA === null || stackB === null) return stackA === stackB;
 
-  if (stackA.stackable_hash !== stackB.stackable_hash) return false;
+	if (stackA.stackable_hash !== stackB.stackable_hash) return false;
 
-  if (ignoreCount) return true;
+	if (ignoreCount) return true;
 
-  return stackA.count === stackB.count;
+	return stackA.count === stackB.count;
 };
 
 export const itemListFromInventories = (
-  mcData: McData,
-  inventories: InventoriesWithLoc
+	mcData: McData,
+	inventories: InventoriesWithLoc
 ): ExtendedItem[] => {
-  const items = new Map<string, ExtendedItem>();
+	const items = new Map<string, ExtendedItem>();
 
-  for (const inventory of inventories) {
-    for (const slot of inventory.slots) {
-      if (!slot) continue;
-      const existing = items.get(slot.stackable_hash);
+	for (const inventory of inventories) {
+		for (const slot of inventory.slots) {
+			if (!slot) continue;
+			const existing = items.get(slot.stackable_hash);
 
-      if (existing) {
-        existing.count += slot.count;
-      } else {
-        items.set(slot.stackable_hash, {
-          prettyPrinted: prettyPrint(mcData, slot),
-          ...slot,
-        });
-      }
-    }
-  }
+			if (existing) {
+				existing.count += slot.count;
+			} else {
+				items.set(slot.stackable_hash, {
+					prettyPrinted: prettyPrint(mcData, slot),
+					...slot,
+				});
+			}
+		}
+	}
 
-  return [...items.values()];
+	return [...items.values()];
 };
