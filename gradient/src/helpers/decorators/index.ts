7a8164a108fb7337685d81d52097bdfc8f5217c8
Sam Mauldin <sam@mauldin.me>
2022-08-27 09:20:57 +0000

    Add basic shulker decorator

--- gradient/src/helpers/decorators/index.ts
+++ gradient/src/helpers/decorators/index.ts
@@ -1,10 +1,12 @@
 import { Decorator } from './types';
 import { anvilNameDecorator } from './anvilName';
 import { enchantmentsDecorator } from './enchantments';
+import { shulkerDecorator } from './shulker';
 
 export * from './types';
 
 export const decorators: Decorator[] = [
   anvilNameDecorator,
   enchantmentsDecorator,
+  shulkerDecorator,
 ];
---
+++ gradient/src/helpers/decorators/shulker.ts
@@ -0,0 +1,13 @@
+import { Decorator } from './types';
+
+export const shulkerDecorator: Decorator = (item, mcData) => {
+  if (!mcData.items.get(item.item_id)?.name?.endsWith('shulker_box'))
+    return null;
+
+  const nbtItemsList: [] | null =
+    item.nbt?.value?.BlockEntityTag?.value?.Items?.value?.value;
+
+  if (!nbtItemsList || nbtItemsList.length == 0) return 'Empty';
+
+  return null;
+};
