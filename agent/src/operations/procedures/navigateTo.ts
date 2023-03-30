23fe8dd3c5c06df9726c1a341369cb00377ab26f
Sam Mauldin <sam@mauldin.me>
2023-03-30 02:04:28 +0000

    Fix floating point issues in navigation

--- agent/src/operations/procedures/navigateTo.ts
+++ agent/src/operations/procedures/navigateTo.ts
@@ -31,19 +31,20 @@ async function flyTo(bot: Bot, destination: depVec3) {
 
   let vector = destination.minus(bot.entity.position);
   let magnitude = vecMagnitude(vector);
-  const normalizedVector = vector.scaled(1 / magnitude);
 
   const allowedTravelTimeMs = 5000 + magnitude * 20;
   let travelTimeExceeded = false;
   let travelTimeTimeout = timers.setTimeout(() => {
-    console.log('Travel time exceeded! Attempting break');
+    console.warn('NAV: Travel time exceeded! Attempting break');
     travelTimeExceeded = true;
   }, allowedTravelTimeMs);
 
   while (true) {
     let nextSegment = bot.entity.position;
     while (true) {
-      const distToEnd = vecMagnitude(destination.minus(nextSegment));
+      const vecToDest = destination.minus(nextSegment);
+      const distToEnd = vecMagnitude(vecToDest);
+      const normalizedVector = vecToDest.scaled(1 / magnitude);
 
       const candidateSegment =
         distToEnd < segmentLength
@@ -74,17 +75,43 @@ async function flyTo(bot: Bot, destination: depVec3) {
   }
 }
 
+const displayLoc = (loc: Location): string => {
+  return `(${loc.vec3.x}, ${loc.vec3.y}, ${loc.vec3.z} in ${loc.dim})`;
+};
+
+const currentPosLoc = (bot: Bot): Location => {
+  const { x, y, z } = bot.player.entity.position;
+  const dim = stringToDim(bot.game.dimension);
+
+  return {
+    vec3: { x, y, z },
+    dim
+  };
+};
+
 export const takePortal = async (vec: Vec3, bot: Bot) => {
   const startingDim = bot.game.dimension;
 
   await setTimeout(700);
   await flyTo(bot, vec3(vec).add(vec3({ x: 0.5, y: 0, z: 0.5 })));
 
-  console.log('At portal, awaiting new dimension...');
+  console.log(
+    `NAV: At portal location: ${displayLoc(
+      currentPosLoc(bot)
+    )}. Waiting for traversal.`
+  );
   while (bot.game.dimension === startingDim) {
     await setTimeout(100);
   }
-  console.log('Portal traversed.');
+  console.log(
+    `NAV: Portal traversed. New location: ${displayLoc(
+      currentPosLoc(bot)
+    )}. Awaiting chunk load.`
+  );
+  while (!bot.world.getColumnAt(bot.entity.position)) {
+    await setTimeout(100);
+  }
+  console.log(`NAV: Chunk loaded. Continuing.`);
 };
 
 const navigateToImpl = async (
@@ -95,8 +122,11 @@ const navigateToImpl = async (
   const { x, y, z } = bot.player.entity.position;
   const dim = stringToDim(bot.game.dimension);
 
-  console.log('=== Starting Navigation ===');
-  console.log('Starting ', { x, y, z }, dim);
+  console.log(
+    `NAV: Starting navigation from ${displayLoc(
+      currentPosLoc(bot)
+    )} to ${displayLoc(destinationLoc)}`
+  );
 
   if (
     destinationLoc.dim === dim &&
@@ -113,7 +143,7 @@ const navigateToImpl = async (
   if (pathResp.type === 'Error') throw new Error('Pathfinding request failed!');
 
   for (const node of pathResp.path) {
-    console.log('Navigating to next node', node);
+    console.log('NAV: Navigating to next node', node);
     if ('Vec' in node) {
       await flyTo(bot, vec3(node.Vec).add(vec3({ x: 0.5, y: 0, z: 0.5 })));
     } else {
@@ -121,7 +151,7 @@ const navigateToImpl = async (
     }
   }
 
-  console.log('=== Navigation Finished ===');
+  console.log(`NAV: Finished`);
 };
 
 export const navigateTo = async (
@@ -137,12 +167,14 @@ export const navigateTo = async (
     const res = await attempt()
       .then(() => true)
       .catch((err) => {
-        console.warn('Navigation attempt failed with ', err);
+        console.warn('NAV: Attempt failed with ', err);
         return false;
       });
 
     if (res) return;
   }
+
+  console.warn('NAV: All attempts failed');
 
   throw new Error('Navigation failed!');
 };
